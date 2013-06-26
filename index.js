484b91d3313a372574efe5fdf5b7ff9b37b91780
TJ Holowaychuk <tj@vision-media.ca>
2013-06-26 15:21:13 -0700

    docs

--- index.js
+++ index.js
@@ -6,12 +6,33 @@
 var fs = require('fs');
 var ms = require('ms');
 
+/**
+ * Expose `Reaper`.
+ */
+
 module.exports = Reaper;
+
+/**
+ * Initialize a new `Reaper` with the given `opts`.
+ *
+ * - `threshold` string or number of milliseconds
+ *
+ * @param {Object} [opts]
+ * @api public
+ */
 
 function Reaper(opts) {
   opts = opts || {};
   this.threshold = ms(opts.threshold || '1 day');
 }
+
+/**
+ * Check if `file` is old.
+ *
+ * @param {String} file
+ * @param {Function} fn
+ * @api public
+ */
 
 Reaper.prototype.old = function(file, fn){
   var ms = this.threshold;
